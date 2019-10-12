c82a90fc2570ac5424b392c7b58a37804de75b33
George Baldwin <georgebaldwin@me.com>
2019-10-12 15:13:48 +0100

    Adjusting flex grid on profile page

--- components/portfoliocard.vue
+++ components/portfoliocard.vue
@@ -1,6 +1,5 @@
 export
 <template>
-  <div class="container">
     <v-card outlined> 
       <v-card-title class="headline">
           <v-icon left>{{ icon }}</v-icon> {{ title }}</v-card-title>
@@ -9,7 +8,6 @@ export
         <p>{{ text }}</p>
       </v-card-text>
     </v-card>
-  </div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,70 +2,68 @@
   <v-layout>
     <v-container fluid>
       <v-row>
-        <v-col>
-          <div class="container">
-            <v-card outlined>
-              <v-list-item three-line class="text-center">
-                <v-list-item-content>
-                  <div style="padding:20px">
-                    <v-avatar size="150">
-                      <img :src="require('@/images/profileimage.jpg')" alt="George Baldwin" />
-                    </v-avatar>
-                  </div>
+        <!-- PICTURE CARD -->
+        <v-col cols="12" xs="12" sm="12" md="3" lg="3">
+          <v-card outlined>
+            <v-list-item three-line class="text-center">
+              <v-list-item-content>
+                <div style="padding:20px">
+                  <v-avatar size="150">
+                    <img :src="require('@/images/profileimage.jpg')" alt="George Baldwin" />
+                  </v-avatar>
+                </div>
 
-                  <v-list-item-title class="headline mb-1">George Baldwin</v-list-item-title>
-                  <v-list-item-subtitle>Software Developer</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
+                <v-list-item-title class="headline mb-1">George Baldwin</v-list-item-title>
+                <v-list-item-subtitle>Software Developer</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
 
-              <div class="text-center" style="padding:15px">
-                <v-btn text icon color="black" href="https://github.com/Geobald" target="_blank">
-                  <v-icon>mdi-github-circle</v-icon>
-                </v-btn>
-                <v-btn
-                  text
-                  icon
-                  color="black"
-                  href="https://twitter.com/GeorgeBaldwin"
-                  target="_blank"
-                >
-                  <v-icon>mdi-twitter-circle</v-icon>
-                </v-btn>
-                <v-btn
-                  text
-                  icon
-                  color="black"
-                  href="https://www.linkedin.com/in/george-baldwin-04578230"
-                  target="_blank"
-                >
-                  <v-icon>mdi-linkedin-box</v-icon>
-                </v-btn>
-              </div>
-            </v-card>
-          </div>
+            <div class="text-center" style="padding:15px">
+              <v-btn text icon color="black" href="https://github.com/Geobald" target="_blank">
+                <v-icon>mdi-github-circle</v-icon>
+              </v-btn>
+              <v-btn
+                text
+                icon
+                color="black"
+                href="https://twitter.com/GeorgeBaldwin"
+                target="_blank"
+              >
+                <v-icon>mdi-twitter-circle</v-icon>
+              </v-btn>
+              <v-btn
+                text
+                icon
+                color="black"
+                href="https://www.linkedin.com/in/george-baldwin-04578230"
+                target="_blank"
+              >
+                <v-icon>mdi-linkedin-box</v-icon>
+              </v-btn>
+            </div>
+          </v-card>
         </v-col>
 
-        <v-col>
-        
-  <div class="container">
-    <v-card outlined> 
-      <v-card-title class="headline">
-          <v-icon left>mdi-account</v-icon> Profile</v-card-title>
-                  <v-divider></v-divider>
-      <v-card-text>
-        <p v-html="profiletext"></p>
-      </v-card-text>
-    </v-card>
-  </div>
-
-
-
+        <!-- PROFILE CARD -->
+         <v-col cols="12" xs="12" sm="12" md="9" lg="9">
+          <v-card outlined>
+            <v-card-title class="headline">
+              <v-icon left>mdi-account</v-icon>Profile
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <p v-html="profiletext"></p>
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
+
+      <!-- NEXT ROW -->
       <v-row>
         <v-col>
           <portfoliocard icon="mdi-xml" title="Skills" text underscore="true" />
         </v-col>
+
         <v-col>
           <portfoliocard
             icon="mdi-information-outline"
@@ -103,13 +101,13 @@ export default {
     };
   },
 
-   mounted() {
-    this.profiletext = portfoliotextservice.getprofiletext()
+  mounted() {
+    this.profiletext = portfoliotextservice.getprofiletext();
   },
 
-  computed:{
+  computed: {
     profiletextresult() {
-       return this.profiletext ? this.profiletext : 'An error occurred';
+      return this.profiletext ? this.profiletext : "An error occurred";
     }
   }
 };
