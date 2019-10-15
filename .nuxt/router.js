0dab1998ed8cc063415cc18e935edc5adccaf455
George Baldwin <georgebaldwin@me.com>
2019-10-15 18:20:36 +0100

    adding temp new pages

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _666a5867 = () => interopDefault(import('../pages/blog.vue' /* webpackChunkName: "pages/blog" */))
+const _087bb6bf = () => interopDefault(import('../pages/projects.vue' /* webpackChunkName: "pages/projects" */))
+const _cb08c79c = () => interopDefault(import('../pages/resume.vue' /* webpackChunkName: "pages/resume" */))
 const _01b41e46 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
@@ -15,6 +18,18 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/blog",
+    component: _666a5867,
+    name: "blog"
+  }, {
+    path: "/projects",
+    component: _087bb6bf,
+    name: "projects"
+  }, {
+    path: "/resume",
+    component: _cb08c79c,
+    name: "resume"
+  }, {
     path: "/",
     component: _01b41e46,
     name: "index"
