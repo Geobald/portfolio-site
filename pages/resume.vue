5d845f7217c1aa3ef6d97801d83e41c71ccdcfb5
George Baldwin <georgebaldwin@me.com>
2019-10-17 09:19:23 +0100

    initial resume design work

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,3 +1,34 @@
 <template>
-    <h3>coming soon...</h3>
+  <v-layout>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12" xs="12" sm="12" md="6">
+          <!-- EXPERIENCE CARD -->
+          <v-card outlined height="100%">
+            <v-card-title class="headline">
+              <v-icon left>mdi-briefcase-outline</v-icon>Experience
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <p>blah</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" xs="12" sm="12" md="6">
+
+          <!-- EDUCATION CARD -->
+          <v-card outlined height="100%">
+            <v-card-title class="headline">
+              <v-icon left>mdi-school-outline</v-icon>Education
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+              <p>blah</p>
+            </v-card-text>
+          </v-card>
+
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-layout>
 </template>
